85ec3cadcd47b962859b477e414e50fae8c2ad5e
Kosti L. <156070560+KostiLaunonen@users.noreply.github.com>
2025-01-17 11:00:57 +0200

    Modal demo complete

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Modal } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const showModal = () => {
+    setModalVisible(true)
+  }
+
+  const hideModal = () => {
+    setModalVisible(false)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable onPress={showModal} >
+        <Text> Show Modal Message </Text>
+      </Pressable>
+            <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(hideModal);
+            }}>
+              <View style = {styles.centeredView}>
+                <View style={styles.modalView}>
+                <Text style={styles.modalText}>Hello world? {'\n'}
+                  This is a modal message.
+                  </Text>
+                <Pressable
+                onPress={() => setModalVisible(hideModal)}>
+                  <Text>Close</Text>
+                </Pressable>
+                </View>
+              </View>
+          </Modal>
     </View>
   );
 }
@@ -16,5 +46,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, centeredView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    modalView: {
+      margin: 25,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      padding: 35,
+      paddingBottom: 50,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+      modalText: {
+      marginBottom: 20,
+      textAlign: 'center'
+    },
 });
